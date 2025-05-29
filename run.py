af92ea4e91f9056d8580fba91e67256fda345cdb
Alexey <vaznoe2024@mail.ru>
2025-05-29 21:33:52 +0300

    v5

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-from db.models import Base, engine
+from db.models import Base, engine, Entry, SessionLocal
 
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    session = SessionLocal()
+    entries = session.query(Entry).order_by(Entry.category).all()
+    session.close()
+    return render_template("index.html", entries=entries)
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
